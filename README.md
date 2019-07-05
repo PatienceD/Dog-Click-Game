db617596b33177d7530385cc76325a91989aebd7
Patience D <patience.deaton2@outlook.com>
2019-07-05 18:06:46 -0400

    in progress, troubleshooting issues

--- README.md
+++
@@ -1,6 +0,0 @@
-# Dog-Click-Game
-
-This is a memory clicking game built with React. This application keeps track of user's score each time they correctly click on an image they haven't clicked before. The user's score will increase with each correct click, and the score will completely reset with an incorrect click. Each time an image is clicked, the images rendered to the page will shuffle themselves in a random order. Good luck!
-
-
-Try it out here: https://patienced.github.io/Dog-Click-Game/
--- dog-click-game/src/App.js
+++ dog-click-game/src/App.js
@@ -1,8 +1,62 @@
-import React from "react";
-import Counter from "./components/Counter";
+import React, {Component} from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import cards from "./cards.json";
 
-function App() {
-  return <Counter />;
+class App extends Component {
+  state = {
+    cards,
+    score: 0,
+    highscore: 0
+  };
+
+  gameOver = () => {
+    if (this.state.score > this.state.highscore){
+      this.setState({highscore: this.state.score}, function(){
+        console.log(this.state.highscore);
+      });
+    }
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert(`Game Over \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.cards.find((o, i) => {
+      if (o.id === id){
+        if(cards[i].count === 0) {
+          cards[i].count = cards[i].count + 1;
+          this.setState({score: this.state.score + 1}, function (){
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true;
+        }else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+
+  render (){
+    return (
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}>Click Game</Header>
+        {this.state.cards.map(card => (
+          <Card
+          clickCount={this.clickCount}
+          id={card.id}
+          key={card.id}
+          image={card.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
