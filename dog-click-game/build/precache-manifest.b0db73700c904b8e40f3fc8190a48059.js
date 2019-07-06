1e629fe23d9a75a1508fa16d29e6fe8e8a002955
Patience D <patience.deaton2@outlook.com>
2019-07-06 12:20:41 -0400

    trying to deploy

---
+++ dog-click-game/build/precache-manifest.b0db73700c904b8e40f3fc8190a48059.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d24431b6a04ef58bda32138103adf3",
+    "url": "/Dog-Click-Game/index.html"
+  },
+  {
+    "revision": "b8d11754ba6b8bcc13b8",
+    "url": "/Dog-Click-Game/static/css/main.dfe965fe.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Dog-Click-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "b8d11754ba6b8bcc13b8",
+    "url": "/Dog-Click-Game/static/js/main.1a93b876.chunk.js"
+  },
+  {
+    "revision": "75f60d8fa6fe17f22e06",
+    "url": "/Dog-Click-Game/static/js/runtime~main.e49b662f.js"
+  }
+]);
